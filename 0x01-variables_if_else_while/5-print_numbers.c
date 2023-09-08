e83333bbe5875428d96d6d2ee1c8ae2dc47cecd3
Iamruth5 <iamruthedegbo@gmail.com>
2023-09-08 11:47:08 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+
+/**
+ * main - program that prints the alphabet in lowercase and upper case
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	{
+	if (letter != 'e' && letter != 'q')
+	putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
+Task 5
+
+#include <stdio.h>
+
+
+/**
+ * main - program that prints the alphabet in lowercase and upper case
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	{
+	if (letter != 'e' && letter != 'q')
+	putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
