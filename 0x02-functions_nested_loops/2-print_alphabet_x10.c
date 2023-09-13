fc09b43e5cc9d48cce87beb20cb166dea01490fe
Iamruth5 <iamruthedegbo@gmail.com>
2023-09-13 14:00:06 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet_x10 - print the alphabest 10 times
- * Return: void here
+ * Return: void 
  */
 
 void print_alphabet_x10(void)
@@ -10,13 +10,13 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-		while (i <= 9)
-		{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-			_putchar('\n');
-			i++;
-		}
+	while (i <= 9)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
 }
